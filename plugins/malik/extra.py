345306d7732bbd620c2b3c0dbe90ee3aa4cbc172
Akshay962 <107416405+Akshay962@users.noreply.github.com>
2022-12-11 17:27:29 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**Join ›› [iPapkornOfficial](https://t.me/+auX77kg8XeI5YTI1)**\n**Made by 🎶›› [](https://t.me/akshaychand10)**'
+        rep = '**Join ›› [iPapkornOfficial](https://t.me/+auX77kg8XeI5YTI1)**\n**Made by 🎶›› [Akshay Chand](https://t.me/akshaychand10)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
