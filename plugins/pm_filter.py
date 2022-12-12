062bca7413d97318b26a5332f02eed613765de39
Akshay962 <107416405+Akshay962@users.noreply.github.com>
2022-12-12 06:35:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -29,7 +29,7 @@ logger.setLevel(logging.ERROR)
 BUTTONS = {}
 SPELL_CHECK = {}
 
-@Client.on_message(filters.group & filters.text & filters.incoming & filters.chat(REQ_GRP))
+@Client.on_message(filters.group & filters.private & filters.text & filters.incoming & filters.chat(REQ_GRP))
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
